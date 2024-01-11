cc9c6a2cb6e136e7ea37801b51e16f363d748724
MDXROBERT <RI207@live.mdx.ac.uk>
2024-01-11 23:39:04 +0000

    Sixth Commit (changed so the file read is not hardcoded and is it asks the user for the file they want  to be used)

--- main.cpp
+++ main.cpp
@@ -12,68 +12,58 @@ std::vector<Book> libraryBooks;
 
 int main()
 {
-
     Librarian librarian(1, 5000, "Librarian Name", "Librarian Address", "librarian@email.com");
-    std::string file = "library_books.csv";
+
+    std::string file;
+    std::cout << "Welcome, Librarian!" << std::endl;
+    std::cout << "Please enter the filename for the library books: ";
+    std::getline(std::cin, file);
     libraryBooks = readBooks(file);
 
     int memberID;
     int bookID;
 
-    std::cout << "Welcome, Librarian!" << std::endl;
-
     while (true)
     {
-
-        std::cout << "What would you like to do next?" << std::endl;
+        std::cout << "\nWhat would you like to do next?" << std::endl;
         std::cout << "1. Add a Member" << std::endl;
         std::cout << "2. Exit" << std::endl;
         std::cout << "3. Display Members" << std::endl;
         std::cout << "4. Issue Book" << std::endl;
-        std::cout << "5. Display Borrowed book" << std::endl;
+        std::cout << "5. Display Borrowed Books" << std::endl;
         std::cout << "6. Return Book" << std::endl;
         std::cout << "Enter your choice: ";
 
         int choice = getIntInput("");
 
-        if (choice == 1)
+        switch(choice)
         {
-            librarian.addMember();
-        }
-        else if (choice == 2)
-        {
-            std::cout << "Exiting the program." << std::endl;
-            break;
-        }
-        else if (choice == 3)
-        {
-            librarian.displayMembers();
-        }
-        else if (choice == 4)
-        {
-            memberID = getIntInput("Enter member ID: ");
-            bookID = getIntInput("Enter book ID: ");
-            librarian.issueBook(memberID, bookID);
-        }
-        else if (choice == 5)
-        {
-            std::cout << "Enter member ID: ";
-            std::cin >> memberID;
-            librarian.displayBorrowedBooks(memberID);
-        }
-        else if (choice == 6)
-        {
-            std::cout << "Enter member ID for the book return: ";
-            std::cin >> memberID;
-            std::cout << "Enter book ID for the book return: ";
-            std::cin >> bookID;
-            librarian.returnBook(memberID, bookID);
-        }
-        else
-        {
-            std::cout << "Invalid choice. Please try again." << std::endl;
+            case 1:
+                librarian.addMember();
+                break;
+            case 2:
+                std::cout << "Exiting the program." << std::endl;
+                return 0;
+            case 3:
+                librarian.displayMembers();
+                break;
+            case 4:
+                memberID = getIntInput("Enter member ID: ");
+                bookID = getIntInput("Enter book ID: ");
+                librarian.issueBook(memberID, bookID);
+                break;
+            case 5:
+                memberID = getIntInput("Enter member ID: ");
+                librarian.displayBorrowedBooks(memberID);
+                break;
+            case 6:
+                memberID = getIntInput("Enter member ID for the book return: ");
+                bookID = getIntInput("Enter book ID for the book return: ");
+                librarian.returnBook(memberID, bookID);
+                break;
+            default:
+                std::cout << "Invalid choice. Please try again." << std::endl;
+                break;
         }
     }
-
-    return 0;
 }
